40a568501768e91f311f887d9864ef1c0b3b7ecd
Daniel-Venera <daniel.venera.programmer@gmail.com>
2020-07-20 18:06:06 +0200

    scroll behavior projects + skills

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 let log = console.log;
+
+let projects = document.querySelectorAll(".projects-row");
+let skillsItems = document.querySelectorAll(".skills-items__item");
+log(skillsItems);
+window.addEventListener("scroll", function () {
+  if (window.innerWidth > 767) {
+    log(window.scrollY);
+    if (window.scrollY > 105) {
+      projects.forEach(function (e) {
+        e.style.transform = "translateY(0)";
+        e.style.opacity = "1";
+      });
+    }
+
+    if (window.scrollY > 1226) {
+      skillsItems.forEach(function (e) {
+        e.style.transform = "translate(0, 0)";
+        e.style.opacity = "1";
+      });
+    }
+  }
+});

ac60347b81ce0f2e95eec514bbd164edc6aa0097
Daniel-Venera <daniel.venera.programmer@gmail.com>
2020-07-21 11:17:11 +0200

    formation link

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ let contact = document.querySelector(".contact-content");
 
 window.addEventListener("scroll", function () {
   if (window.innerWidth > 767) {
-    log(window.scrollY);
     if (window.scrollY > 105) {
       projects.forEach(function (e) {
         e.style.transform = "translateY(0)";
@@ -16,23 +15,22 @@ window.addEventListener("scroll", function () {
       });
     }
 
-    if (window.scrollY > 1500) {
+    if (window.scrollY > 1600) {
       skillsItems.forEach(function (e) {
         e.style.transform = "translate(0, 0)";
         e.style.opacity = "1";
       });
     }
-    if (window.scrollY > 1800) {
+    if (window.scrollY > 1900) {
       formationItems.forEach(function (e) {
-        log(e);
         e.style.transform = "rotateY(0)";
       });
     }
-    if (window.scrollY > 2000) {
+    if (window.scrollY > 2100) {
       aboutContent.style.transform = "translateX(0)";
       aboutContent.style.opacity = "1";
     }
-    if (window.scrollY > 2400) {
+    if (window.scrollY > 2500) {
       contact.style.transform = "translateX(0)";
       contact.style.opacity = "1";
     }

a8e827403c729e87bfb1949b6eadd2654f3040e9
Daniel-Venera <daniel.venera.programmer@gmail.com>
2020-07-21 09:06:19 +0200

    arrow up + project title bolded + margin project mobile + skills height

--- js/script.js
+++ js/script.js
@@ -2,10 +2,9 @@ let log = console.log;
 
 let projects = document.querySelectorAll(".projects-row");
 let skillsItems = document.querySelectorAll(".skills-items__item");
-log(skillsItems);
+
 window.addEventListener("scroll", function () {
   if (window.innerWidth > 767) {
-    log(window.scrollY);
     if (window.scrollY > 105) {
       projects.forEach(function (e) {
         e.style.transform = "translateY(0)";
@@ -13,7 +12,7 @@ window.addEventListener("scroll", function () {
       });
     }
 
-    if (window.scrollY > 1226) {
+    if (window.scrollY > 1300) {
       skillsItems.forEach(function (e) {
         e.style.transform = "translate(0, 0)";
         e.style.opacity = "1";
